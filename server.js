65822dafbace3b58bf801975cde1cd43a7518522
Lucio <junqueirafreitasl@gmail.com>
2026-03-13 19:18:39 -0300

    aumentar limite upload

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ require('dotenv').config()
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(express.static('public'))
 
 app.use('/api/auth', require('./routes/auth'))
